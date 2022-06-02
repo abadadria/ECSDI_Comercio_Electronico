24897b4cd8da39aece88e73ec60d8132f11e4525
raul-sampietro <raul.sampietro@estudiantat.upc.edu>
2022-06-02 23:03:17 +0200

    Directorio + BuscadorProductos funciona

---
+++ src/Agentes/BuscadorProductos.py
@@ -0,0 +1,285 @@
+# -*- coding: utf-8 -*-
+"""
+
+Esqueleto de agente usando los servicios web de Flask
+
+/Stop es la entrada que para el agente
+
+Tiene una funcion AgentBehavior1 que se lanza como un thread concurrente
+
+Asume que el agente de registro esta en el puerto 9000
+
+"""
+
+import logging
+from multiprocessing import Process, Queue
+import socket
+from SPARQLWrapper import SPARQLWrapper
+import argparse
+
+from flask import Flask, request, render_template
+from rdflib import Graph, RDF, Namespace, RDFS, Literal
+from rdflib.namespace import FOAF
+
+from AgentUtil.ACL import ACL
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+from AgentUtil.ACLMessages import *
+from AgentUtil.Logging import config_logger
+from AgentUtil.DSO import DSO
+from AgentUtil.Util import gethostname
+
+from decimal import Decimal
+
+
+__author__ = 'adria'
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir',
+                    default=None,
+                    help="Direccion del servicio de directorio")
+parser.add_argument('--port',
+                    type=int,
+                    help="Puerto de comunicacion del agente")
+parser.add_argument('--verbose',
+                    help="Genera un log de la comunicacion del servidor web",
+                    action='store_true',
+                    default=False)
+
+# Logging
+logger = config_logger(level=1)
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+if args.dir is None:
+    raise NameError('A Directory Service addess is needed')
+else:
+    diraddress = args.dir
+
+# Configuration stuff
+if args.port is None:
+    port = 9010
+else:
+    port = args.port
+
+hostname = socket.gethostname()
+
+agn = Namespace("http://www.agentes.org#")
+
+CEO = Namespace("http://www.semanticweb.org/samragu/ontologies/comercio-electronico#")
+
+# Contador de mensajes
+mss_cnt = 0
+
+# Datos del Agente
+BuscadorProductos = Agent('BuscadorProductos',
+                       CEO.BuscadorProductos,
+                       'http://%s:%d/comm' % (hostname, port),
+                       'http://%s:%d/Stop' % (hostname, port))
+
+ServicioDirectorio = Agent('ServicioDirectorio',
+                        CEO.ServicioDirectorio,
+                        '%s/register' % (diraddress),
+                        '%s/Stop' % (diraddress))
+
+# Global triplestore graph
+products_graph = Graph()
+
+cola1 = Queue()
+
+# Flask stuff
+app = Flask(__name__)
+
+if not args.verbose:
+    logger = logging.getLogger('werkzeug')
+    logger.setLevel(logging.ERROR)
+
+@app.route("/comm")
+def comunicacion():
+    """
+    Entrypoint de comunicacion
+    """
+
+    def buscarProductos():
+        gr = Graph()
+        gr.namespace_manager.bind('rdf', RDF)
+        gr.namespace_manager.bind('ceo', CEO)
+        
+        for s, p, o in gm.triples((None, RDF.type, CEO.LineaBusqueda)):
+            cantidad = gm.value(s, CEO.cantidad)
+            categoria = gm.value(s, CEO.categoria)
+            precio_max = gm.value(s, CEO.precio_max)
+            precio_min = gm.value(s, CEO.precio_min)
+            
+            
+            ''' NO FUNCIONA
+            
+            # FILTER (?precio <= '""" + str(precio_max) + """' && ?precio >= '""" + str(precio_min) + """' && ?categoria = '""" + categoria + """')
+            
+            query = """PREFIX ceo: <http://www.semanticweb.org/samragu/ontologies/comercio-electronico#>
+                    SELECT ?Oferta ?Producto ?categoria ?precio
+                    WHERE {
+                        ?Oferta rdf:type ceo:Oferta .
+                        ?Oferta ceo:precio ?precio .
+                        ?Producto ceo:ofertado_en ?Oferta .
+                        ?Producto ceo:categoria ?categoria .
+                        FILTER (?precio <= '""" + str(precio_max) + """' && ?precio >= '""" + str(precio_min) + """' && ?categoria = '""" + categoria + """')
+                    }"""
+            
+            graph = products_graph.query(query, initNs= {'rdf', RDF})
+            
+            print('graph despues de query)
+            for row in graph:
+                print(row)
+            print('graph despues de query)    
+            
+            '''
+            
+            for s, p, o in products_graph.triples((None, RDF.type, CEO.Producto)):                
+                categoriap = products_graph.value(s, CEO.categoria)
+                categoriaOk = False
+                if categoriap == categoria:
+                     categoriaOk = True
+                            
+                if categoriaOk:
+                    oferta = products_graph.value(s, CEO.ofertado_en)
+                    precio = products_graph.value(oferta, CEO.precio)
+                    precioOk = False
+                    if Decimal(precio) < int(precio_max) and Decimal(precio) > int(precio_min):
+                        precioOk = True
+                    if precioOk:
+                        gr.add((s, RDF.type, CEO.Producto))
+                        gr.add((s, CEO.categoria, categoriap))
+                        gr.add((oferta, RDF.type, CEO.Oferta))
+                        gr.add((oferta, CEO.precio, precio))
+                        gr.add((s, CEO.ofertado_en, oferta))
+
+        return gr
+
+
+    message = request.args['content']
+    gm = Graph()
+    gm.parse(data=message, format='xml')
+
+    msgdic = get_message_properties(gm)
+
+    # Comprobamos que sea un mensaje FIPA ACL
+    if not msgdic:
+        # Si no es, respondemos que no hemos entendido el mensaje
+        gr = build_message(Graph(),
+                           ACL['not-understood'],
+                           sender=BuscadorProductos.uri)
+    else:
+        # Obtenemos la performativa
+        if msgdic['performative'] != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            gr = build_message( Graph(),
+                                ACL['not-understood'],
+                                sender=BuscadorProductos.uri)
+        else:
+            # Extraemos el objeto del contenido que ha de ser una accion de la ontologia
+            # de registro
+            content = msgdic['content']
+
+            # Averiguamos el tipo de la accion
+            accion = gm.value(subject=content, predicate=RDF.type)
+
+            # Aqui realizariamos lo que pide la accion
+            # Por ahora simplemente retornamos un Inform-done
+            if accion == CEO.BuscarProductos:
+                gr = buscarProductos()
+            else:
+                gr = build_message( Graph(),
+                                ACL['not-understood'],
+                                sender=BuscadorProductos.uri)
+            
+    return gr.serialize(format='xml')
+    
+
+
+@app.route("/Stop")
+def stop():
+    """
+    Entrypoint que para el agente
+
+    :return:
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+    pass
+    
+
+
+def agentbehavior1(cola):
+    """
+    Un comportamiento del agente
+
+    :return:
+    """
+    pass
+    
+    
+def setup():
+    products_graph.parse('product.ttl', format='turtle')
+
+    # Registro al ServicioDirectorio
+    gm = Graph()
+    gm.namespace_manager.bind('rdf', RDF)
+    gm.namespace_manager.bind('ceo', CEO)
+
+    ra = CEO.registraragente
+    gm.add((ra, RDF.type, CEO.RegistrarAgente))
+    gm.add((CEO.RegistrarAgente, RDFS.subClassOf, CEO.Accion))
+    gm.add((CEO.Accion, RDFS.subClassOf, CEO.Comunicacion))
+
+    a = CEO.agente
+    gm.add((a, RDF.type, CEO.BuscadorProductos))
+    gm.add((CEO.BuscadorProductos, RDFS.subClassOf, CEO.Agente))
+    gm.add((a, CEO.direccion, Literal(BuscadorProductos.address)))
+    gm.add((a, CEO.uri, BuscadorProductos.uri))
+    gm.add((ra, CEO.con_agente, a))
+
+    logger.info('Peticion de registro al ServicioDirectorio')
+    logger.info(gm.serialize(format='turtle'))
+
+    msg = build_message(gm,
+                        ACL.request,
+                        sender=BuscadorProductos.uri,
+                        receiver=ServicioDirectorio.uri,
+                        content=ra)
+    
+    gr = send_message(msg, ServicioDirectorio.address)
+
+    logger.info(gr.serialize(format='turtle'))
+
+    if (None, ACL.performative, ACL.confirm) in gr:
+        print('\n  * Registro de agente CONFIRMADO')
+    else:
+        print('\n  * Registro de agente NO confirmado')
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha los behaviors
+    ab1 = Process(target=agentbehavior1, args=(cola1,))
+    ab1.start()
+    
+    setup()
+
+    print('\nRunning on https://' + str(hostname) + ':' + str(port) + '/ (Press CTRL+C to quit)\n')
+
+    # Ponemos en marcha el servidor
+    app.run(host=hostname, port=port)
+
+    # Esperamos a que acaben los behaviors
+    ab1.join()
+    print('The End')
---
+++ src/Agentes/ServicioDirectorio.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+"""
+filename: SimpleDirectoryAgent
+
+Antes de ejecutar hay que añadir la raiz del proyecto a la variable PYTHONPATH
+
+Agente que lleva un registro de otros agentes
+
+Utiliza un registro simple que guarda en un grafo RDF
+
+El registro no es persistente y se mantiene mientras el agente funciona
+
+Las acciones que se pueden usar estan definidas en la ontología
+directory-service-ontology.owl
+
+
+@author: javier
+"""
+
+from multiprocessing import Process, Queue
+import argparse
+import logging
+
+from flask import Flask, request, render_template
+from rdflib import Graph, RDF, Namespace, RDFS, Literal
+from rdflib.namespace import FOAF
+
+from AgentUtil.ACL import ACL
+from AgentUtil.FlaskServer import shutdown_server
+from AgentUtil.Agent import Agent
+from AgentUtil.ACLMessages import build_message, get_message_properties
+from AgentUtil.Logging import config_logger
+from AgentUtil.DSO import DSO
+from AgentUtil.Util import gethostname
+import socket
+
+__author__ = 'raul'
+
+# Definimos los parametros de la linea de comandos
+parser = argparse.ArgumentParser()
+parser.add_argument('--verbose', help="Genera un log de la comunicacion del servidor web", action='store_true',
+                    default=False)
+
+# Logging
+logger = config_logger(level=1)
+
+# parsing de los parametros de la linea de comandos
+args = parser.parse_args()
+
+# Configuration stuff
+port = 9000
+hostaddr = hostname = socket.gethostname()
+
+print('DS Hostname =', hostaddr)
+
+# Directory Service Graph
+dsgraph = Graph()
+
+# Vinculamos todos los espacios de nombre a utilizar
+CEO = Namespace("http://www.semanticweb.org/samragu/ontologies/comercio-electronico#")
+
+dsgraph.bind('acl', ACL)
+dsgraph.bind('rdf', RDF)
+dsgraph.bind('rdfs', RDFS)
+dsgraph.bind('foaf', FOAF)
+dsgraph.bind('dso', DSO)
+dsgraph.bind('ceo', CEO)
+
+agn = Namespace("http://www.agentes.org#")
+ServicioDirectorio = Agent('ServicioDirectorio',
+                       agn.Directory,
+                       'http://%s:%d/Register' % (hostaddr, port),
+                       'http://%s:%d/Stop' % (hostaddr, port))
+app = Flask(__name__)
+
+if not args.verbose:
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+mss_cnt = 0
+
+
+@app.route("/register")
+def register():
+    """
+    Entry point del agente que recibe los mensajes de registro
+    La respuesta es enviada al retornar la funcion,
+    no hay necesidad de enviar el mensaje explicitamente
+
+    Asumimos una version simplificada del protocolo FIPA-request
+    en la que no enviamos el mesaje Agree cuando vamos a responder
+
+    :return:
+    """
+
+    def process_register():
+        # Si la hay extraemos el nombre del agente (FOAF.name), el URI del agente
+        # su direccion y su tipo
+
+        logger.info('Peticion de registro')
+        logger.info(gm.serialize(format='turtle'))
+
+        agn_type = gm.value(subject=CEO.agente, predicate=RDF.type)
+        agn_uri = gm.value(subject=CEO.agente, predicate=CEO.uri)
+        agn_adr = gm.value(subject=CEO.agente, predicate=CEO.direccion)
+
+        dsgraph.add((agn_adr, RDF.type, agn_type))
+        dsgraph.add((agn_adr, CEO.uri, agn_uri))
+        dsgraph.add((agn_adr, CEO.direccion, agn_adr))
+
+        logger.info('Grafo del ServicioDirectorio')
+        logger.info(dsgraph.serialize(format='turtle'))
+
+        # Generamos un mensaje de respuesta
+        return build_message(Graph(),
+                             ACL.confirm,
+                             sender=ServicioDirectorio.uri,
+                             receiver=agn_uri,
+                             msgcnt=mss_cnt)
+
+
+        '''
+        agn_add = gm.value(subject=content, predicate=DSO.Address)
+        agn_name = gm.value(subject=content, predicate=FOAF.name)
+        agn_uri = gm.value(subject=content, predicate=DSO.Uri)
+        agn_type = gm.value(subject=content, predicate=DSO.AgentType)
+
+        # Añadimos la informacion en el grafo de registro vinculandola a la URI
+        # del agente y registrandola como tipo FOAF.Agent
+        dsgraph.add((agn_uri, RDF.type, FOAF.Agent))
+        dsgraph.add((agn_uri, FOAF.name, agn_name))
+        dsgraph.add((agn_uri, DSO.Address, agn_add))
+        dsgraph.add((agn_uri, DSO.AgentType, agn_type))
+
+        # Generamos un mensaje de respuesta
+        return build_message(Graph(),
+                             ACL.confirm,
+                             sender=ServicioDirectorio.uri,
+                             receiver=agn_uri,
+                             msgcnt=mss_cnt)
+        '''
+    def process_search():
+        # Asumimos que hay una accion de busqueda que puede tener
+        # diferentes parametros en funcion de si se busca un tipo de agente
+        # o un agente concreto por URI o nombre
+        # Podriamos resolver esto tambien con un query-ref y enviar un objeto de
+        # registro con variables y constantes
+
+        # Solo consideramos cuando Search indica el tipo de agente
+        # Buscamos una coincidencia exacta
+        # Retornamos el primero de la lista de posibilidades
+
+        logger.info('Peticion de busqueda')
+
+        agn_type = gm.value(subject=content, predicate=DSO.AgentType)
+        rsearch = dsgraph.triples((None, DSO.AgentType, agn_type))
+        if rsearch is not None:
+            agn_uri = next(rsearch)[0]
+            agn_add = dsgraph.value(subject=agn_uri, predicate=DSO.Address)
+            gr = Graph()
+            gr.bind('dso', DSO)
+            rsp_obj = agn['Directory-response']
+            gr.add((rsp_obj, DSO.Address, agn_add))
+            gr.add((rsp_obj, DSO.Uri, agn_uri))
+            return build_message(gr,
+                                 ACL.inform,
+                                 sender=ServicioDirectorio.uri,
+                                 msgcnt=mss_cnt,
+                                 receiver=agn_uri,
+                                 content=rsp_obj)
+        else:
+            # Si no encontramos nada retornamos un inform sin contenido
+            return build_message(Graph(),
+                                 ACL.inform,
+                                 sender=ServicioDirectorio.uri,
+                                 msgcnt=mss_cnt)
+
+    global dsgraph
+    global mss_cnt
+    # Extraemos el mensaje y creamos un grafo con él
+    message = request.args['content']
+    gm = Graph()
+    gm.parse(data=message, format='xml')
+
+
+    msgdic = get_message_properties(gm)
+
+    # Comprobamos que sea un mensaje FIPA ACL
+    if not msgdic:
+        # Si no es, respondemos que no hemos entendido el mensaje
+        gr = build_message(Graph(),
+                           ACL['not-understood'],
+                           sender=ServicioDirectorio.uri,
+                           msgcnt=mss_cnt)
+    else:
+        # Obtenemos la performativa
+        if msgdic['performative'] != ACL.request:
+            # Si no es un request, respondemos que no hemos entendido el mensaje
+            gr = build_message(Graph(),
+                               ACL['not-understood'],
+                               sender=ServicioDirectorio.uri,
+                               msgcnt=mss_cnt)
+        else:
+            # Extraemos el objeto del contenido que ha de ser una accion de la ontologia
+            # de registro
+            content = msgdic['content']
+            # Averiguamos el tipo de la accion
+            accion = gm.value(subject=content, predicate=RDF.type)
+
+            # Accion de registro
+            if accion == CEO.RegistrarAgente:
+                gr = process_register()
+            # Accion de busqueda
+            elif accion == CEO.BuscarAgente:
+                gr = process_search()
+            # No habia ninguna accion en el mensaje
+            else:
+                gr = build_message(Graph(),
+                                   ACL['not-understood'],
+                                   sender=ServicioDirectorio.uri,
+                                   msgcnt=mss_cnt)
+    mss_cnt += 1
+ 
+    return gr.serialize(format='xml')
+
+
+@app.route('/info')
+def info():
+    """
+    Entrada que da informacion sobre el agente a traves de una pagina web
+    """
+    global dsgraph
+    global mss_cnt
+
+    return render_template('info.html', nmess=mss_cnt, graph=dsgraph.serialize(format='turtle'))
+
+
+@app.route("/stop")
+def stop():
+    """
+    Entrada que para el agente
+    """
+    tidyup()
+    shutdown_server()
+    return "Parando Servidor"
+
+
+def tidyup():
+    """
+    Acciones previas a parar el agente
+
+    """
+
+
+if __name__ == '__main__':
+    # Ponemos en marcha el servidor Flask
+    app.run(host=hostname, port=port, debug=True)
+
+    logger.info('The End')
